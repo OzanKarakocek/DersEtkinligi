2b48acf30268599c9996e37573b7b2ea464b18c0
Ozan Karaköcek <ozankarakocek@outlook.com>
2024-03-27 11:15:51 +0300

    Proje dosyası ekle.

---
+++ DersEtkinligi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TemelCSharpProjesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba, dünya!");
+
+            
+            Console.Write("Lütfen adınızı girin: ");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Merhaba, " + isim + "!");
+
+           
+            Console.Write("Yaşınızı girin: ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (yas < 18)
+            {
+                Console.WriteLine("Gençsiniz!");
+            }
+            else
+            {
+                Console.WriteLine("Yetişkinsiniz!");
+            }
+
+           
+            Console.WriteLine("Görüşmek üzere, " + isim + "!");
+
+          
+            Console.WriteLine("Çıkış yapmak için bir tuşa basın...");
+            Console.ReadKey();
+        }
+    }
+}
